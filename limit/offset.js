fcd5706248187eb26abd75c5f9f5b05e3934fc44
HBFLEX <happybanda@dyuni.ac.mw>
2023-10-03 17:22:35 +0200

    add limit example

---
+++ limit/offset.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'example',
+});
+
+const sql = "SELECT * FROM user LIMIT 2 OFFSET 2";
+
+conn.connect((err) => {
+    if(err) throw err;
+    conn.query(sql, (err, res, fields) => {
+        if(err) throw err;
+        console.log(res);
+    });
+});
