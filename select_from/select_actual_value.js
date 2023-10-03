48809165c6deb3eb8e18bd49381bd513d7f43c00
HBFLEX <happybanda@dyuni.ac.mw>
2023-10-03 16:45:01 +0200

    add select from example

---
+++ select_from/select_actual_value.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'example',
+});
+
+const sql = "SELECT * FROM user";
+
+conn.connect((err) => {
+    if(err) throw err;
+    conn.query(sql, (err, res, fields) => {
+        if(err) throw err;
+        console.log(res[3].username);
+    });
+});
+
+
+
+
